59e1005202f3a2a737b7b47bdec91583e4f6ae00
Samuel Frossard <samuel.frossard79@gmail.com>
2023-11-01 22:25:45 +0100

    le dernier commit que j'ai oublié de faire sur le tp rest

--- booking/booking.py
+++ booking/booking.py
@@ -31,25 +31,26 @@ def get_bookings_by_userid(userid):
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking(userid):
     req = request.get_json()
-    validity = requests.get('http://localhost:3201/bookings/verification', data=req).json()
+    print("aaa");
+    validity = requests.get('http://localhost:3201/bookings/verification', json=req).json()
+    print("bbb");
     if not validity["validity"]:
         return make_response(jsonify({"error":"schedule doesn't exist"}),400)
+    print("ccc");
     user_found = False
 
+    print("ddd");
     for user_bookings in bookings:
         if str(user_bookings["userid"]) == str(userid):
             user_found = True
-            print("user_found = true")
+            print("User found")
             for schedule in user_bookings["dates"]:
                 if schedule["date"] == req["date"]:
-                    print("date trouvée")
+                    print("date found")
                     for movie in schedule["movies"]:
                         if movie == req["movieid"]:
                             return make_response(jsonify({"error": "booking already registered"}), 400)
-                    print("avant schedule[movies]", schedule["movies"])
                     schedule["movies"].append([req["movieid"]])
-                    print("après schedule[movies]", schedule["movies"])
-
                     res = make_response(jsonify({"message": "booking added"}), 200)
                     return res
             new_date = {
@@ -57,9 +58,9 @@ def add_booking(userid):
                 "movies": [req["movieid"]]
             }
             user_bookings["dates"].append(new_date)
-            res = make_response(jsonify({"message":"booking added"}),200)
+            res = make_response(jsonify({"message": "booking added"}), 200)
     if not user_found:
-        print("user_found = false")
+        print("User not found")
         new_date = {
             "date": req["date"],
             "movies": [req["movieid"]]
@@ -70,15 +71,13 @@ def add_booking(userid):
         }
         bookings.append(new_user_bookings)
         res = make_response(jsonify({"message": "new user and booking added"}), 200)
-    return res
+        return res
 
 
 @app.route("/bookings/verification", methods=['GET'])
 def check_booking_validity():
     new_movie = request.get_json()
-    print("new_movie", new_movie)
-    schedule = requests.get('http://172.16.132.100:3202/showtimes').json()
-    print(schedule)
+    schedule = requests.get('http://localhost:3202/showtimes').json()
     for single_schedule in schedule:
         if single_schedule["date"] == new_movie["date"]:
             for movie in single_schedule["movies"]:
@@ -86,25 +85,11 @@ def check_booking_validity():
                     return make_response(jsonify({"validity": True}), 200)
     return make_response(jsonify({"validity": False}), 200)
 
-"""
-{
-                "date": "20151201",
-                "movies": [
-                    "267eedb8-0f5d-42d5-8f43-72426b9fb3e6"
-                ]
-            }
-"""
-"""
-    for single_schedule in schedule:
-        if single_schedule["date"] == booking["date"]:
-            for movie in single_schedule["movies"]:
-                if movie == booking["movieid"]:
-                    return True
-    return False
-"""
+
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s"%(PORT))
+    app.run(host=HOST, port=PORT)
+
 
 
 
--- user/user.py
+++ user/user.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request, jsonify, make_response
 import requests
 import json
-from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
-
 PORT = 3203
 HOST = '0.0.0.0'
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+    users = json.load(jsf)["users"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return make_response("<h1 style='color:blue'>Welcome to the User service!</h1>", 200)
+    return make_response("<h1 style='color:blue'>Welcome to the User service!</h1>", 200)
 
 @app.route("/users", methods=['GET'])
 def get_json():
@@ -28,7 +27,7 @@ def get_user_by_id(userid):
             return res
     return make_response(jsonify({"error":"bad input parameter"}),400)
 
-@app.route("/users/bookings/<userid>", methods=['GET', 'POST'])
+@app.route("/users/bookings/<userid>", methods=['GET'])
 def get_bookings_by_userid(userid):
     bookings = requests.get('http://localhost:3201/bookings/'+userid).json()
     res = make_response(jsonify(bookings["dates"]),200)
@@ -36,22 +35,17 @@ def get_bookings_by_userid(userid):
 
 @app.route("/users/movies/<userid>", methods=['GET'])
 def get_movies_by_userid(userid):
-    print("userid", userid)
     user_bookings_request = requests.get('http://localhost:3203/users/bookings/'+userid)
     user_bookings = user_bookings_request.json()
-    print("user_bookings", user_bookings)
     if user_bookings_request.status_code == 200:
         moviesInfo = {
             "movies":[]
         }
         for booking in user_bookings:
             for movieid in booking["movies"]:
-                print("movieid", movieid)
-                print('http://localhost:3200/movies/'+movieid)
                 movie_request = requests.get('http://localhost:3200/movies/'+movieid)
                 movie = movie_request.json()
                 if movie_request.status_code ==200:
-                    print("movie", movie)
                     moviesInfo["movies"].append(movie)
         return make_response(jsonify(moviesInfo),200)
     return make_response(jsonify({"error": "no booking found for user" + userid}), 400)
